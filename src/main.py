d54faf05cf64ce857b8590db90d1892d52321953
Tautvidas <t.9b5as@gmail.com>
2012-01-04 17:00:30 +0200

    Updated class skeleton, added hello world app from wikipedia for testing

---
+++ src/main.py
@@ -0,0 +1,27 @@
+
+class BrainfuckMachine:
+    """Brainfuck interpreter"""
+
+    def __init__(self):
+        self.tape = [0 for i in range(30000)]
+        self.cellPointer = 0
+        self.codePointer = 0
+        self.code = ''
+
+    def run(self):
+        return
+
+    def movePointer(self, direction):
+        return
+
+    def changeValue(self, action):
+        return
+
+    def printPointer(self):
+        return
+
+    def getPointer(self):
+        return
+
+    def loop(self):
+        return
